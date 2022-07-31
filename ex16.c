0f66c1d7b343415c510c6337d27daf6d4b7b9365
Miroslav Kurka <59745102+miroslavkurka@users.noreply.github.com>
2022-07-31 23:19:40 +0200

    Update ex16.c

--- ex16.c
+++ ex16.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Create struct with properties
 struct Person {
   char *name;
   int age;
@@ -11,9 +12,11 @@ struct Person {
 };
 
 struct Person *Person_create(char *name, int age, int height, int weight) {
-
+  // allocate memory with size of struct Person
   struct Person *who = malloc(sizeof(struct Person));
+  // check if it is valid pointer
   assert(who != NULL);
+  // duplicate the string
   who->name = strdup(name);
   who->age = age;
   who->height = height;
@@ -23,6 +26,7 @@ struct Person *Person_create(char *name, int age, int height, int weight) {
 void Person_destroy(struct Person *who) {
 
   assert(who != NULL);
+  // Free memory
   free(who->name);
   free(who);
 }
@@ -33,11 +37,14 @@ void Person_print(struct Person *who) {
   printf("\tHeight %d\n", who->height);
   printf("\tWeight %d\n", who->weight);
 }
-int main(*argc, *argv[]) {
+int main(int *argc, char *argv[]) {
+  // create two persons joe and frank
   struct Person *joe = Person_create("Joe Alex", 32, 64, 140);
-  struct Person *frank = Person_create("Frank Blank", 20, 72, 180)
-      printf("Joe is at memory location %p:\n", joe) Person_print(joe);
-  printf("Frank is at memory location %p:\n", frank) Person_print(frank);
+  struct Person *frank = Person_create("Frank Blank", 20, 72, 180);
+  printf("Joe is at memory location %p:\n", joe);
+  Person_print(joe);
+  printf("Frank is at memory location %p:\n", frank);
+  Person_print(frank);
 
   // make everyone age 20
   joe->age += 20;
